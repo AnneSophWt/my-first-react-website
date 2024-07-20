4b9c833aee7aa61091f89dd27cfba1f0924f8533
Anne-Sophie Woite <anne-sophie.woite@AnneSophiesAir.fritz.box>
2024-07-20 14:23:48 +0200

    Your commit message here

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,14 +1,15 @@
 // src/components/About.tsx
 import React from 'react';
 import "./About.css";
+import aboutImage from '../assets/aboutImage.jpg';
 
 
 const About: React.FC = () => {
   return (
     <section id="about">
       <h2>About Me</h2>
-      <img src='About.jpg'></img>
-      <p>I'm Anne-Sophie, 25 years old living in cologne. I have a bachelor degree in media and marketing management and currently working as a production assistent and in the festival management. In september I'm going to start my master in UX design and hopefully become one in the furute. I started with coding in march and I have a lot of fun with it. I would love to become a frontend developer too.  </p>
+      <img src={aboutImage} alt='About Me' class='about-image'></img>
+      <p>I'm Anne-Sophie, 25 years old living in cologne. I have a bachelor degree in media and marketing management and currently working as a production assistent and in the festival management. In september I'm going to start my master in UX design and hopefully become one in the furute. I started with coding in march and since then it is one of my favorite hobbys to do, I would love to work in the tech industry! I have many different hobbys, from baking and cooking to reading books/manga, drawing and generally be creative. </p>
     </section>
   );
 };
--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -2,18 +2,42 @@
 import React from 'react';
 import "./Home.css";
 
+interface Project {
+  title: string; 
+  description: string;
+  link: string;
+}
+
+const projects: Project[] = [
+  {
+    title: "snake game 🐍",
+    description: "This project is still under develeopment.",
+    link: "project-link-1",
+  },
+  {
+    title: "Special birthday card for a special friend 💝",
+    description: "Project descritpion goes here.",
+    link: "https://birthdaycard26.web.app",
+  },
+  {
+    title: "chess game",
+    description: "This project is as good as finished. There a still a few bugs that need to be fixed.",
+    link: "https://chess-85331.web.app",
+  },
+];
+
 const Projects: React.FC = () => {
   return (
     <section id="projects">
       <h2>Projects</h2>
-      <div className="project">
-        <h3>Project Title</h3>
-        <p>Project description goes here.</p>
-        <a href="project-link">View Project</a>
-      </div>
-      {/* Add more projects as needed */}
-    </section>
+      {projects.map((project, index) => (
+        <div className='project' key={index}>
+          <h3>{project.title}</h3>
+          <p>{project.description}</p>
+          <a href={project.link}>View Project</a>
+        </div>
+      ))}
+      </section>
   );
 };
-
 export default Projects;
