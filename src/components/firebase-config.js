c84f5900d38bb597bda2df339e8a5893ce5a9ace
Anne-Sophie Woite <anne-sophie.woite@AnneSophiesAir.fritz.box>
2024-07-21 22:01:03 +0200

    removed node_modules

---
+++ src/components/firebase-config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyApvmXj_PlrePOoCOETUEH-77lV-4EoPFY",
+  authDomain: "myfirstreacwebsite.firebaseapp.com",
+  projectId: "myfirstreacwebsite",
+  storageBucket: "myfirstreacwebsite.appspot.com",
+  messagingSenderId: "27440956625",
+  appId: "1:27440956625:web:b6874162b4b05efc1171f0"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
